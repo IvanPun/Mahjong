ead942c07b1587d544869e0329dc8a048ba796b9
IvanPun <whitetiger123asd@gmail.com>
2022-05-28 20:23:31 +0800

    update

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-	function fetch_page() {
-        fetch('data/read_data.py')
-        .then(Response => Response.json())
-        .then(json => {
+function fetch_page() {
+       fetch('data/read_data.py')
+       .then(Response => Response.json())
+       .then(json => {
 
-          s='';
-          for(i=0;i<json.length;i++){
-          	s += `<tr>`;
-            s += `<th>${json[i].flight}</th><td>${json[i].start}</td><td>${json[i].end}</td><td>${json[i].stime}</td><td>${json[i].etime}</td><td style="color:blue;cursor: pointer;" onclick="dayone(this,'${json[i].flight}','${json[i].start}','${json[i].end}','${json[i].stime}','${json[i].etime}')">預約</td><td style="color:blue;cursor: pointer;" onclick="daytwo(this,'${json[i].flight}','${json[i].start}','${json[i].end}','${json[i].stime}','${json[i].etime}')">預約</td><td style="color:blue;cursor: pointer;" onclick="daythree(this,'${json[i].flight}','${json[i].start}','${json[i].end}','${json[i].stime}','${json[i].etime}')">預約</td><td><input type="text" class="form-control" aria-label="1" aria-describedby="addon-wrapping" value="1" id="${json[i].flight}"></td>`;
-            s += `</tr>`;
-          }
+         s='';
+         for(i=0;i<json.length;i++){
+         	num = i+1;
+         	s += `<tr>`;
+         	s += `<th>${num}</th><td>${json[i].name}</td><td>${json[i].data}</td>`;
+         	s += `</tr>`;
+         }
 
-          output = document.getElementById('output').innerHTML = s;
-        })
-    }
+         output = document.getElementById('output').innerHTML = s;
+       })
+   }
+
+fetch_page();

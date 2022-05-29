d4705123ffa0a52b4a7c856179173f026da8bcf7
IvanPun <whitetiger123asd@gmail.com>
2022-05-29 14:48:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 function fetch_page() {
+       fetch('data/read_data.py')
+       .then(Response => Response.json())
+       .then(json => {
 
-      const fs = require('fs');
-      
-      // create a JSON object
-      const user = {
-          "id": 1,
-          "name": "John Doe",
-          "age": 22
-      };
-      
-      // convert JSON object to string
-      const data = JSON.stringify(user, null, 4);
-      
-      // write JSON string to a file
-      fs.writeFile('user.json', data, (err) => {
-          if (err) {
-              throw err;
-          }
-          console.log("JSON data is saved.");
-      });
+         s='';
+         for(i=0;i<json.length;i++){
+         	num = i+1;
+         	s += `<tr>`;
+         	s += `<th>${num}</th><td>${json[i].name}</td><td>${json[i].data}</td>`;
+         	s += `</tr>`;
+         }
+
+         output = document.getElementById('output').innerHTML = s;
+       })
    }
 
 fetch_page();
-

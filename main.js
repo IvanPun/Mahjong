1c1948778b92d00b115e75b641296c7694bd67ee
IvanPun <whitetiger123asd@gmail.com>
2022-06-13 12:55:25 +0800

    update

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ function add_people(){
           <div class="card-body">
             <div class="mb-3">
               <label for="exampleFormControlInput1" class="form-label"><h5>賭鬼的稱號</h5></label>
+              <button type="button" class="btn btn-danger" onclick="delete_form()" id="delete_form"><i class="bi bi-person-x-fill"></i></button>
               <input type="text" class="form-control" id="exampleFormControlInput1">
             </div>
           </div>
         </div>`
   document.getElementById("add-people").innerHTML += s;
+}
+
+function delete_form(){
+  s = `<div class="card">
+          <div class="card-body">
+            <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label"><h5>賭鬼的稱號</h5></label>
+              <button type="button" class="btn btn-danger" onclick="delete_form()" id="delete_form"><i class="bi bi-person-x-fill"></i></button>
+              <input type="text" class="form-control" id="exampleFormControlInput1">
+            </div>
+          </div>
+        </div>`
+  document.getElementById("add-people").innerHTML -= s;
 }
